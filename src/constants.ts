d749024d488e2aad0665033c66db2d686473375d
Elton Souza <elton.souza1515@gmail.com>
2024-05-12 18:16:49 -0300

    feat: create constants data - available units (based in static data) - default unit active

---
+++ src/constants.ts
@@ -0,0 +1,16 @@
+export const AVAILABLE_UNITS = [
+  {
+    name: 'Apex Unit',
+    value: 'apex',
+  },
+  {
+    name: 'Tobias Unit',
+    value: 'tobias',
+  },
+  {
+    name: 'Jaguar Unit',
+    value: 'jaguar',
+  },
+]
+
+export const INITIAL_UNIT_ACTIVE = AVAILABLE_UNITS[0]
