5526bfcdec1ac0c37d2c2cc8e4d6e0756978845b
Elton Souza <elton.souza1515@gmail.com>
2024-05-12 18:34:18 -0300

    feat: mount app structure and wrapper the components with unit context

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import Header from './components/Header'
+import UnitContent from './components/UnitContent'
+import UnitContextProvider from './context/UnitContext'
+
 function App() {
-  return <></>;
+  return (
+    <main className="h-screen bg-gray-400  overflow-hidden">
+      <UnitContextProvider>
+        <Header />
+        <section className="h-full p-2  overflow-hidden">
+          <UnitContent />
+        </section>
+      </UnitContextProvider>
+    </main>
+  )
 }
 
-export default App;
+export default App
