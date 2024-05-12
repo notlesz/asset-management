822759de492200881a27cbed2cb8e4594a827e85
Elton Souza <elton.souza1515@gmail.com>
2024-05-12 18:35:17 -0300

    feat: extends the new style settings in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{jsx,tsx,js,ts, html}"],
+  content: ['./src/**/*.{jsx,tsx,js,ts, html}'],
   theme: {
     extend: {
       backgroundColor: {
-        main: "#17192D",
+        main: '#17192D',
       },
+      borderColor: {
+        card: 'rgba(216, 223, 230, 1)',
+      },
+    },
+    keyframes: {
+      rotate180: {
+        '0%': { transform: 'rotate(0deg)' },
+        '100%': { transform: 'rotate(180deg)' },
+      },
+    },
+    animation: {
+      rotate180: 'rotate180 1s ease-in-out',
     },
   },
   plugins: [],
-};
+}
