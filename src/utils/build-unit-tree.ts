e9a2af35cd11c3b069ffab1e48880b663ad7f2cb
Elton Souza <elton.souza1515@gmail.com>
2024-05-12 18:20:39 -0300

    feat: create helper to build relations in tree with locations and assets data

---
+++ src/utils/build-unit-tree.ts
@@ -0,0 +1,43 @@
+import { Asset, Location, SensorType, Status } from '../types'
+
+export interface TreeNode {
+  id: string
+  name: string
+  parentId: string | null
+  locationId?: string | null
+  children?: TreeNode[]
+  sensorType?: SensorType
+  status?: Status
+}
+
+export default function buildTree(locations: Location[], assets: Asset[]): TreeNode[] {
+  const nodes: TreeNode[] = locations
+    .map((loc) => ({
+      ...loc,
+      children: [],
+    }))
+    .concat(
+      assets.map((asset) => ({
+        ...asset,
+        parentId: asset.locationId || asset.parentId,
+        children: [],
+      }))
+    )
+
+  const rootNodes: TreeNode[] = []
+  const nodesById: { [key: string]: TreeNode } = {}
+
+  nodes.forEach((node) => {
+    nodesById[node.id] = node
+    if (node.parentId === null) {
+      rootNodes.push(node)
+    } else {
+      const parentNode = nodesById[node.parentId]
+      if (parentNode) {
+        parentNode.children = parentNode.children ? [...parentNode.children, node] : [node]
+      }
+    }
+  })
+
+  return rootNodes
+}
