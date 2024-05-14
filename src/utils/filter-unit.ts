3ae1885744099da46a5a8e46d29a498a69c7b171
Elton Souza <elton.souza1515@gmail.com>
2024-05-13 22:52:20 -0300

    feat: create helper to filter unit based on activeFilter and search

---
+++ src/utils/filter-unit.ts
@@ -0,0 +1,40 @@
+import { Filter } from '../context/type'
+import { Sensor, Status } from '../types'
+import { TreeNode } from './build-unit-tree'
+
+export default function filterUnit(
+  units: TreeNode[],
+  filter: { search: string; activeFilter: Filter | null }
+): TreeNode[] {
+  const { activeFilter, search } = filter
+
+  const isCriticalFilter = activeFilter === Filter.CRITICAL
+  const isEnergySensorFilter = activeFilter === Filter.ENERGY_SENSOR
+
+  if (!activeFilter && !search) return units
+
+  return units.reduce((acc, curr) => {
+    const { name, children, sensorType, status } = curr
+
+    const matchEnergySensor = isEnergySensorFilter && sensorType === Sensor.ENERGY
+    const matchCriticalFilter = isCriticalFilter && status === Status.ALERT
+    const matchSearch = search && name.toLowerCase().includes(search.toLowerCase())
+
+    const hasChildren = children && children.length > 0
+
+    const allMatch = matchEnergySensor || matchCriticalFilter || matchSearch
+
+    if (allMatch) {
+      acc.push(curr)
+    }
+
+    if (hasChildren) {
+      const filteredChildren = filterUnit(children, filter)
+      if (filteredChildren.length > 0) {
+        acc.push({ ...curr, children: filteredChildren })
+      }
+    }
+
+    return acc
+  }, [] as TreeNode[])
+}
