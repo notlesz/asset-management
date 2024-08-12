1c9ee3b8c57557c2e4b97dda8e19676b0641f201
Elton Souza <elton.souza1515@gmail.com>
2024-08-12 10:38:41 -0300

    feat: create filters component

---
+++ src/components/Filters.tsx
@@ -0,0 +1,42 @@
+import { AiOutlineExclamationCircle, AiOutlineThunderbolt } from 'react-icons/ai'
+import { Filter } from '../context/type'
+import useUnitContext from '../hooks/useCompanyContext'
+import Button from './Button'
+import { cn } from '../utils/cn'
+
+export default function Filters() {
+  const { activeFilter, handleActiveFilter } = useUnitContext()
+
+  const isEnergySensorFilter = activeFilter === 'ENERGY_SENSOR'
+  const isCriticalFilter = activeFilter === 'CRITICAL'
+  return (
+    <div className="flex items-center gap-2">
+      <Button
+        variant={isEnergySensorFilter ? 'primary' : 'secondary'}
+        size="md"
+        onClick={() => handleActiveFilter(Filter.ENERGY_SENSOR)}
+      >
+        <AiOutlineThunderbolt
+          size={16}
+          className={cn('text-blue-500', {
+            'text-white': isEnergySensorFilter,
+          })}
+        />
+        Sensor de Energia
+      </Button>
+      <Button
+        variant={isCriticalFilter ? 'primary' : 'secondary'}
+        size="md"
+        onClick={() => handleActiveFilter(Filter.CRITICAL)}
+      >
+        <AiOutlineExclamationCircle
+          size={16}
+          className={cn('text-blue-500', {
+            'text-white': isCriticalFilter,
+          })}
+        />
+        Crítico
+      </Button>
+    </div>
+  )
+}
