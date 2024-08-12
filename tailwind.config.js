f1a46d4c50c37c2d2c79007191cf65c9bbe1dfb9
Elton Souza <elton.souza1515@gmail.com>
2024-08-12 10:31:14 -0300

    chore: add react query and tweaks in tailwind config and global styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,21 +3,24 @@ export default {
   content: ['./src/**/*.{jsx,tsx,js,ts, html}'],
   theme: {
     extend: {
+      colors: {
+        main: '#17192D',
+      },
       backgroundColor: {
         main: '#17192D',
       },
       borderColor: {
         card: 'rgba(216, 223, 230, 1)',
       },
-    },
-    keyframes: {
-      rotate180: {
-        '0%': { transform: 'rotate(0deg)' },
-        '100%': { transform: 'rotate(180deg)' },
+      keyframes: {
+        rotate180: {
+          '0%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(180deg)' },
+        },
       },
-    },
-    animation: {
-      rotate180: 'rotate180 1s ease-in-out',
+      animation: {
+        rotate180: 'rotate180 1s ease-in-out',
+      },
     },
   },
   plugins: [],
