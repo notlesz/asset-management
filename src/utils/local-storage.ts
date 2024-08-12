ec520c5f3144aa018c4b15299163c7b00e86b50e
Elton Souza <elton.souza1515@gmail.com>
2024-08-12 10:33:55 -0300

    chore: remove unused files

--- src/utils/local-storage.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-
-export const getLocaStorageItem = (key: string) => localStorage.getItem(key)
-
-export const setLocalStorageItem = (key: string, value: any) => {
-  localStorage.setItem(key, value)
-}
