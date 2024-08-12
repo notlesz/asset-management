781e4fa90f19fb57e7d5c419c38212751288123e
Elton Souza <elton.souza1515@gmail.com>
2024-08-12 10:39:58 -0300

    feat: create loading component

---
+++ src/components/Loading.tsx
@@ -0,0 +1,28 @@
+import { tv, VariantProps } from 'tailwind-variants'
+import { cn } from '../utils/cn'
+import { HTMLProps } from 'react'
+
+const loadingStyles = tv({
+  base: 'animate-spin border-solid border-white border-t-main rounded-full',
+  variants: {
+    size: {
+      small: 'w-[20px] h-[20px] border-[3px]',
+      medium: 'w-[40px] h-[40px] border-[6px]',
+      large: 'w-[60px] h-[60px] border-[8px]',
+    },
+  },
+  defaultVariants: {
+    size: 'medium',
+  },
+})
+
+type ButtonVariants = VariantProps<typeof loadingStyles>
+
+interface LoadingProps {
+  className?: HTMLProps<HTMLElement>['className']
+  size?: ButtonVariants['size']
+}
+
+export default function Loading({ size, className }: LoadingProps) {
+  return <span className={cn(loadingStyles({ size }), className)} />
+}
